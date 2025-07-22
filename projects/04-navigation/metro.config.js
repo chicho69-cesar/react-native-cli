80badf201f9efe8cbe1f9b68fcf902cabb58b818
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-07-22 08:41:10 -0600

    :wrench: Configure navigation app

--- projects/04-navigation/metro.config.js
+++ projects/04-navigation/metro.config.js
@@ -1,11 +1,8 @@
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = wrapWithReanimatedMetroConfig(mergeConfig(getDefaultConfig(__dirname), config));
