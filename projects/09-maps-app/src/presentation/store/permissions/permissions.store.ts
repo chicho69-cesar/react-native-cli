46b5ab14e72a2842a79f8404f5a9ed19bef8e182
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-07-27 11:52:46 -0600

    :sparkles: Add global store

---
+++ projects/09-maps-app/src/presentation/store/permissions/permissions.store.ts
@@ -0,0 +1,28 @@
+import { create } from 'zustand'
+import { checkLocationPermission, requestLocationPermission } from '../../../actions/permissions/location'
+import { PermissionStatus } from '../../../infrastructure/interfaces/permissions'
+
+interface PermissionsState {
+  locationStatus: PermissionStatus
+
+  requestLocationPermission: () => Promise<PermissionStatus>
+  checkLocationPermission: () => Promise<PermissionStatus>
+}
+
+export const usePermissionsStore = create<PermissionsState>((set) => ({
+  locationStatus: 'undetermined',
+
+  requestLocationPermission: async () => {
+    const status = await requestLocationPermission()
+    set({ locationStatus: status })
+
+    return status
+  },
+
+  checkLocationPermission: async () => {
+    const status = await checkLocationPermission()
+    set({ locationStatus: status })
+
+    return status
+  }
+}))
