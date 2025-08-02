c7f9f1d3fe1ea017416540ae40517ee4008aeb68
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-08-01 19:39:34 -0600

    :sparkles: Add adapters and configs

---
+++ projects/10-teslo-shop/src/config/api/teslo-api.ts
@@ -0,0 +1,30 @@
+import { API_URL_ANDROID, API_URL_IOS, API_URL as PROD_URL, STAGE } from '@env'
+import axios from 'axios'
+import { Platform } from 'react-native'
+import { StorageAdapter } from '../adapters/storage.adapter'
+
+export const API_URL = STAGE === 'prod'
+  ? PROD_URL
+  : Platform.OS === 'android'
+    ? API_URL_ANDROID
+    : API_URL_IOS
+
+const tesloApi = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  }
+})
+
+tesloApi.interceptors.request.use(async (config) => {
+  const token = await StorageAdapter.getItem('token')
+
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`
+  }
+
+  return config
+})
+
+export default tesloApi
